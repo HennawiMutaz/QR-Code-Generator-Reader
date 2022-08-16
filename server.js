ecac1aeaf7f24275c0247a9bd0bde698c54ee932
Mutaz Hennawi <mutazhennawi360@gmail.com>
2022-08-17 00:18:53 +0300

    initial commit

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(express.static('public'));
 app.set('view engine', 'ejs');
 app.get('/', (req,res) => {
     res.render('index');
-});
+}); 
 
 app.post('/scan', (req,res) => {
     const text = req.body.text;
